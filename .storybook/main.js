f0988e693d5f5c707ef8010421a8583ddff7935f
Matt Styles <matt.styles@gmail.com>
2020-12-08 13:31:09 +0000

    Update react and storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,36 @@
+const merge = require("webpack-merge");
+const path = require("path");
+
+const styleSheetConfig = require("../webpack/configs/styleSheetConfig");
+
 module.exports = {
   stories: ["../stories/**/*.stories.tsx", "../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-actions", "@storybook/addon-links"],
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
         {
-          loader: require.resolve("ts-loader")
+          loader: require.resolve("ts-loader"),
         },
         // Optional
         {
-          loader: require.resolve("react-docgen-typescript-loader")
-        }
-      ]
+          loader: require.resolve("react-docgen-typescript-loader"),
+        },
+      ],
     });
     config.resolve.extensions.push(".ts", ".tsx");
+
+    // Replace existing CSS rule and add one with module support
+    const cssRuleIndex = config.module.rules.findIndex(
+      (rule) => String(rule.test).indexOf("css") !== -1
+    );
+    if (cssRuleIndex !== -1) {
+      config.module.rules.splice(cssRuleIndex, 1);
+    }
+
+    config = merge(config, styleSheetConfig(process.env, { mode: config.mode }));
+
     return config;
-  }
+  },
 };
